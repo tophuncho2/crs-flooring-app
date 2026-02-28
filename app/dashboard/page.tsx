262afe0e69db17c1a7a1ac17679c9cad2651e43a
tophuncho2 <tophuncho2@gmail.com>
2026-02-27 23:42:54 -0500

    OK

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,14 @@
 import { getServerSession } from "next-auth"
 import { authOptions } from "../api/auth/[...nextauth]/route"
 import UserMenu from "./user-menu"
+import { redirect } from "next/navigation"
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions)
+
+if (!session) {
+  redirect("/login")
+}
 
   return (
     <div className="min-h-screen bg-black text-white relative">
