3e114c2ba05143c4395d03e0f7eb4e9ea7c4b3a4
tophuncho <tophuncho2@gmail.com>
2026-05-01 13:59:35 -0400

    z

---
+++ packages/db/prisma/migrations/20260501175827_add_cut_log_unit_snapshots/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "flooring_cut_log" ADD COLUMN     "itemCoverageUnitAbbrev" TEXT,
+ADD COLUMN     "itemCoverageUnitName" TEXT,
+ADD COLUMN     "stockUnitAbbrev" TEXT,
+ADD COLUMN     "stockUnitName" TEXT;
