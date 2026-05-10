9f7a1d133b855a4fdaa204e101b8211ca2ea8447
tophuncho <tophuncho2@gmail.com>
2026-05-09 21:11:02 -0400

    import %

---
+++ packages/db/prisma/migrations/20260509210000_drop_import_entry_percent/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `percent` on the `flooring_import_entry` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "flooring_import_entry" DROP COLUMN "percent";
